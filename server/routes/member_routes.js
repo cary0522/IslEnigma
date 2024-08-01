94011310ed51448269ec0eac298d118957edc683
XU,ZHE-CHENG <thisterryhome@gmail.com>
2024-08-01 18:24:15 +0800

    refactor:0801 finish info

--- server/routes/member_routes.js
+++ server/routes/member_routes.js
@@ -2,8 +2,10 @@ const express = require('express');
 const router = express.Router();
 const login_controllers = require('../controllers/login_controllers');
 const register_controllers = require('../controllers/register_controllers');
-
+const memberInfo_controllers = require('../controllers/memberInfo_controllers');
 router.post('/login', login_controllers.login);
 router.post('/register', register_controllers.register);
+router.get('/memberInfo', memberInfo_controllers);
+router.post('/')
 
 exports.router = router;
