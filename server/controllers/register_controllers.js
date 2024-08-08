77f1808e5636cf24ab8e74eecd733de47426e623
XU,ZHE-CHENG <thisterryhome@gmail.com>
2024-08-08 11:46:17 +0800

    refactor: update CORS origin URLs to match frontend development server

--- server/controllers/register_controllers.js
+++ server/controllers/register_controllers.js
@@ -6,15 +6,18 @@ const register_controllers = {
   register: async (req, res) => {
     try {
       const memberData = req.body;
+      console.log('memberData:', memberData);
+      console.log(memberData);
       const checkMember = await register_Model.read(memberData.account);
       if (checkMember) {
+        console.log('帳號已存在,是否要登入?');
         return res.status(409).json({
           error: '帳號已存在,是否要登入?'
         });;
       }
       const createMember = await register_Model.create(memberData);
       if (createMember) {
-        res.status(201).send('Member created');
+        res.status(201).json({ message: '' });
         console.log('Member created');
       } else {
         res.status(400).json({error: '帳號註冊失敗，請洽系統管理員'});
