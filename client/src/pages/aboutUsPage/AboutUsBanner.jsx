b5d2ba7406248c45e15a7502983d59bff4df65f4
sunnisacoder <funsunny1107@gmail.com>
2024-08-10 18:53:12 +0800

    aboutUs:done

--- client/src/pages/aboutUsPage/AboutUsBanner.jsx
+++ client/src/pages/aboutUsPage/AboutUsBanner.jsx
@@ -1,15 +1,28 @@
 import React, { useState } from "react";
+import LogoArea from "/src/components/common/LogoArea.jsx";
+import imgLogo from "/images/logo.png";
+
 
 function AboutUsBanner() {
-	return (
-		<div className="wrap">
+    return (
+        <div className="wrap">
             <div className="bannerImgBox">
                 <img src="/aboutUs/banner.png" />
             </div>
-            <div className="logoBox">
-                <img src="/aboutUs/logo.png" />
+            <div className="divOutside">
+                <div className="logoBox">
+                    <img src="/aboutUs/logo.png" />
+                </div>
+            </div>
+            <div className="divOutside">
+                <div className="divPageName">
+                    <span className="linePageName"></span>
+                    <h3>關於我們</h3>
+                    <span className="linePageName"></span>
+                </div>
             </div>
         </div>
-	);
+
+    )
 }
 export default AboutUsBanner;
