65219c355d50c49110405d1f82e3d136339fc5d9
kuei <cary0522@gmail.com>
2024-08-12 16:12:27 +0800

    merge main to kuei

--- client/src/pages/ticketpage/DateCalendar.jsx
+++ client/src/pages/ticketpage/DateCalendar.jsx
@@ -1,18 +1,48 @@
 import React from "react";
 import Calendar from "react-calendar";
+import DatePicker from "react-date-picker";
 
-function DateCalendar({ date, dateShow, selectDate, changeView , tileClassName }) {
+import "react-date-picker/dist/DatePicker.css";
+import "react-calendar/dist/Calendar.css";
+
+function DateCalendar({
+	date,
+	dateShow,
+	selectDate,
+	changeView,
+	tileClassName,
+}) {
 	if (dateShow) {
 		return (
-			<div className="calendarArea" onClick={(e)=>{e.stopPropagation()}}>
-				<Calendar
+			<div
+				className="calendarArea"
+				onClick={(e) => {
+					e.stopPropagation();
+				}}
+			>
+				{/* <Calendar
 					value={date}
 					calendarType={"gregory"}
 					defaultValue={new Date()}
 					minDate={new Date()}
 					onChange={selectDate}
 					onActiveStartDateChange={changeView}
-                    tileClassName={tileClassName}
+					tileClassName={tileClassName}
+				/> */}
+				<DatePicker
+					value={date}
+					onChange={selectDate}
+					clearIcon={false}
+					closeCalendar={false}
+					disableCalendar={false}
+					isOpen={true}
+					shouldCloseCalendar={({ reason }) => reason !== "outsideAction"}
+					locale="en-us"
+					// calendarType={"gregory"}
+					// defaultValue={new Date()}
+					// minDate={new Date()}
+					// onActiveStartDateChange={changeView}
+					// tileClassName={tileClassName}
 				/>
 			</div>
 		);
