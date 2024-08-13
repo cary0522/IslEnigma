24eee5fb4f960c65a8160f6dce34fe3f6d280fd4
Chloe0a1z <gilachi0210@gmail.com>
2024-08-13 09:36:36 +0800

    0813

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,38 +1,36 @@
+import { Navigate, Outlet, Route, Routes, useLocation } from "react-router-dom";
 
-import { Navigate, Outlet, Route, Routes, useLocation } from "react-router-dom"
+import "./App.css";
+import Layout from "./components/layout/Layout";
+import ShoppingCart from "./pages/shoppingCartPage/ShoppingCartPage";
+import CheckOutPage from "./pages/checkoutPage/CheckOutPage";
 
-import "./App.css"
-import Layout from "./components/layout/Layout"
-import ShoppingCart from "./pages/shoppingCartPage/ShoppingCartPage"
-import CheckOutPage from "./pages/checkoutPage/CheckOutPage"
-
-import ShoppingSuccessPage from "./pages/shoppingSuccessPage/ShoppingSuccessPage"
-import CartLayout from "./components/layout/CartLayout"
-import Restaurant from "./pages/restaurantPage/Restaurant"
-import Rooms from "./pages/roomPage/Rooms"
-import Login from "./pages/loginPage/Login"
-import Register from "./pages/registerPage/Register"
+import ShoppingSuccessPage from "./pages/shoppingSuccessPage/ShoppingSuccessPage";
+import CartLayout from "./components/layout/CartLayout";
+import Restaurant from "./pages/restaurantpage/Restaurant";
+import Rooms from "./pages/roomPage/Rooms";
+import Login from "./pages/loginPage/Login";
+import Register from "./pages/registerPage/Register";
 //toast
-import { ToastContainer, toast } from "react-toastify"
-import "react-toastify/dist/ReactToastify.css"
-import { useAuthContext } from "./context/AuthContext"
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { useAuthContext } from "./context/AuthContext";
 
 import MapPage from "./pages/mappage";
 import TicketPage from "./pages/ticketpage";
-import RestaurantPage from "./pages/restaurantpage"
+import RestaurantPage from "./pages/restaurantpage";
 import Homepage from "./pages/homepage";
-import BoatPage from './pages/boatpage'
+import BoatPage from "./pages/boatpage";
 import InfoEventPage from "./pages/infoEventPage";
 import InfoInfoPage from "./pages/infoInfoPage";
 import InfoFAQPage from "./pages/infoFAQPage";
 import RoomMorePage from "./pages/roomMorePage";
-import RestaurantPaginationPage from "./pages/restaurantPaginationPage"
-import Hello from "./Hello"
+import RestaurantPaginationPage from "./pages/restaurantPaginationPage";
+import Hello from "./Hello";
 import AboutUsPage from "./pages/aboutUsPage";
 
-
 function App() {
-  const { member } = useAuthContext()
+  const { member } = useAuthContext();
 
   return (
     <>
@@ -65,24 +63,29 @@ function App() {
           </Route>
           <Route path="restaurant" element={<Restaurant />} />
           <Route path="rooms" element={<Rooms />} />
-           <Route path="/mappage" element={< MapPage />} />
-        <Route path="/ticketpage" element={< TicketPage />} />
-        <Route path="/restaurantpage" element={< RestaurantPage />} />
-        <Route path="/restaurantpage/pagination/:urlId" element={< RestaurantPaginationPage />} />
-        <Route path="/boatpage" element={< BoatPage />} />
-        <Route path="/infoEventPage" element={<InfoEventPage />} />
-        <Route path="/infoEventPage/infoInfoPage" element={<InfoInfoPage />} />
-        <Route path="/infoEvent/infoFAQPage" element={<InfoFAQPage />} />
-        {/* <Route path="/roomPage" element={< RoomPage />}/> */}
-        <Route path="/room/roomMorePage" element={<RoomMorePage />} />
-        <Route path="/homepage" element={< Homepage />} />
-        <Route path="/about-us" element={< AboutUsPage />} />
+          <Route path="/mappage" element={<MapPage />} />
+          <Route path="/ticketpage" element={<TicketPage />} />
+          <Route path="/restaurantpage" element={<RestaurantPage />} />
+          <Route
+            path="/restaurantpage/pagination/:urlId"
+            element={<RestaurantPaginationPage />}
+          />
+          <Route path="/boatpage" element={<BoatPage />} />
+          <Route path="/infoEventPage" element={<InfoEventPage />} />
+          <Route
+            path="/infoEventPage/infoInfoPage"
+            element={<InfoInfoPage />}
+          />
+          <Route path="/infoEvent/infoFAQPage" element={<InfoFAQPage />} />
+          {/* <Route path="/roomPage" element={< RoomPage />}/> */}
+          <Route path="/room/roomMorePage" element={<RoomMorePage />} />
+          <Route path="/homepage" element={<Homepage />} />
+          <Route path="/about-us" element={<AboutUsPage />} />
         </Route>
       </Routes>
       <ToastContainer />
     </>
   );
-
 }
 
 export default App;
