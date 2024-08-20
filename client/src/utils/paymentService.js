fa5535ed9850fd7ad2a1280404239bf6f08bde42
Luffa26728238 <8an987@gmail.com>
2024-08-20 20:03:32 +0800

    0820-fixed-popup

--- client/src/utils/paymentService.js
+++ client/src/utils/paymentService.js
@@ -14,8 +14,6 @@ export class PaymentService {
   }
 
   async pay() {
-    console.log(this.#paymentMethod)
-
     switch (this.#paymentMethod) {
       case "stripe":
         await this.#payWithStripe()
@@ -73,8 +71,7 @@ export class PaymentService {
           "您的訂單已付款，可能是之前版本問題導致，請重新註冊新帳號在測試看看！或是將資料庫中customer_order的status改為PAID"
         )
 
-      console.log(res.data)
-      const paymentUrl = res.data.info.paymentUrl?.web
+      const paymentUrl = res.data.info?.paymentUrl?.web
 
       window.location.href = paymentUrl
     } catch (error) {
